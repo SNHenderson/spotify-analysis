5ee461fdb0df9724c01cbc746717411b96be58eb
Samuel Henderson <snhender@ncsu.edu>
2018-06-28 18:54:28 -0400

    Remove debug line

--- app.py
+++ app.py
@@ -94,7 +94,6 @@ def data_view():
     figures = []
     for col in df.columns:
         if(col != 'name'):
-            print(df[col])
             fig = df[col].astype(float).sort_values().plot(kind = 'box', legend=True).get_figure()
             png_output = BytesIO()
             fig.savefig(png_output, format='png')
